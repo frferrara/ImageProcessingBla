5478d83ccbe0818a6dcdf806d4f564e79d001761
Francesco Ferrara <fr.ferrara@gmail.com>
2012-05-03 18:14:32 +0200

    Context for the state machine
    Signed-off-by: Francesco Ferrara <fr.ferrara@gmail.com>

---
+++ ImageProcessing/ImageProcessing.cpp
@@ -0,0 +1,11 @@
+/*
+ * ImageProcessing.cpp
+ *
+ *  Created on: May 3, 2012
+ *      Author: frferrara
+ */
+
+
+#include "ImageProcessing.hpp"
+
+
---
+++ ImageProcessing/ImageProcessing.hpp
@@ -0,0 +1,14 @@
+/*
+ * ImageProcessing.hpp
+ *
+ *  Created on: May 3, 2012
+ *      Author: frferrara
+ */
+
+#ifndef IMAGEPROCESSING_HPP_
+#define IMAGEPROCESSING_HPP_
+
+
+
+
+#endif /* IMAGEPROCESSING_HPP_ */
